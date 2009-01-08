c31ed081f4d462c76b7c8ac0650d17bb508df15c
Al-Faisal El-Dajani <faisal@naruto.(none)>
2009-01-08 14:22:49 +0200

    Adding simple api requests

---
+++ lib/ikbis/simple/video.rb
@@ -0,0 +1,24 @@
+module Ikbis
+  module Simple
+
+    class Video < Ikbis::Simple::Base
+      base_uri 'ikbis.com/api/video'
+
+      def info(id, response='xml')
+        get("info/#{id}/#{response}")
+      end
+
+      def comments(id, response='xml')
+        get("comments/#{id}/#{response}")
+      end
+
+      def in_category(category, response='xml')
+        get("in_category/#{category}/#{response}")
+      end
+
+      def has_tag(tag, response='xml')
+        get("has_tag/#{tag}/#{response}")
+      end
+    end
+  end
+end
